ae0991f974f05f0d97b01531ca58c4aba17a35f1
jiyemun <jym6853@gmail.com>
2025-07-17 16:22:01 +0900

    update tsconfig.json

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+export const ROLES = {
+    VIEWER: "viewer",
+    MANAGER: "manager",
+    ADMIN: "admin",
+};
+export const LICENSES = {
+    BASIC: "basic",
+    PRO: "pro",
+    ENTERPRISE: "enterprise",
+};
+export const REGION_KEY = {
+    QA: "staging",
+    GLOBAL: "kr",
+    COM2US: "com2us",
+};
