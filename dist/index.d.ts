bac14909df56537eab18e1f5b13feb1ea57a2807
jiyemun <jym6853@gmail.com>
2025-08-18 16:24:39 +0900

    update add permission

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -25,8 +25,9 @@ export type ThemeType = (typeof ThemeKey)[keyof typeof ThemeKey];
 export interface ModuleProps {
     region: RegionType;
     role: Role;
-    license: LicenseDetail;
+    license?: LicenseDetail | null;
     theme?: ThemeType;
+    permission: Permission[];
 }
 export type RegionType = (typeof REGION_KEY)[keyof typeof REGION_KEY];
 export declare const REGION_KEY: {
@@ -35,3 +36,31 @@ export declare const REGION_KEY: {
     readonly GLOBAL: "global";
     readonly COM2US: "com2us";
 };
+export interface Permission {
+    name: MenuType;
+    hasReadPermission: boolean;
+    hasWritePermission: boolean;
+}
+export declare const MENU_KEY: {
+    readonly DASHBOARD: "DASHBOARD";
+    readonly AUTH_HISTORY: "AUTH_HISTORY";
+    readonly RATE_OF_CHANGE: "RATE_OF_CHANGE";
+    readonly RATE_OF_CHANGE_STATISTICS: "RATE_OF_CHANGE_STATISTICS";
+    readonly RATE_OF_CHANGE_SETTING: "RATE_OF_CHANGE_SETTING";
+    readonly ALWAYS_VALID_ON: "ALWAYS_VALID_ON";
+    readonly C4_AUTH: "C4_AUTH";
+    readonly C4_AUTH_HISTORY: "C4_AUTH_HISTORY";
+    readonly C4_AUTH_SERVER_INVALID: "C4_AUTH_SERVER_INVALID";
+    readonly C4_AUTH_FLEXIBLE_POLICY: "C4_AUTH_FLEXIBLE_POLICY";
+    readonly C4_AUTH_WATCH_IP_AUTH: "C4_AUTH_WATCH_IP_AUTH";
+    readonly C4_AUTH_BLACK: "C4_AUTH_BLACK";
+    readonly DEVELOP: "DEVELOP";
+    readonly MODULE_VERSION: "MODULE_VERSION";
+    readonly MODULE_VERSION_OPENED: "MODULE_VERSION_OPENED";
+    readonly MODULE_VERSION_REPORTED: "MODULE_VERSION_REPORTED";
+    readonly APP_GUARDING_HISTORY: "APPGUARDING_HISTORY";
+    readonly HIDDEN_LOG: "HIDDEN_LOG";
+    readonly USER: "user";
+    readonly TEST: "test";
+};
+export type MenuType = (typeof MENU_KEY)[keyof typeof MENU_KEY];
--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,9 @@ export type ThemeType = (typeof ThemeKey)[keyof typeof ThemeKey];
 export interface ModuleProps {
   region: RegionType;
   role: Role;
-  license: LicenseDetail;
+  license?: LicenseDetail | null;
   theme?: ThemeType;
+  permission: Permission[]
 }
 
 export type RegionType = (typeof REGION_KEY)[keyof typeof REGION_KEY];
@@ -44,3 +45,34 @@ export const REGION_KEY = {
   GLOBAL: "global",
   COM2US: "com2us",
 } as const;
+
+export interface Permission {
+  name: MenuType;
+  hasReadPermission: boolean;
+  hasWritePermission: boolean;
+}
+
+export const MENU_KEY = {
+  DASHBOARD: "DASHBOARD",
+  AUTH_HISTORY: "AUTH_HISTORY",
+  RATE_OF_CHANGE: "RATE_OF_CHANGE",
+  RATE_OF_CHANGE_STATISTICS: "RATE_OF_CHANGE_STATISTICS",
+  RATE_OF_CHANGE_SETTING: "RATE_OF_CHANGE_SETTING",
+  ALWAYS_VALID_ON: "ALWAYS_VALID_ON",
+  C4_AUTH: "C4_AUTH",
+  C4_AUTH_HISTORY: "C4_AUTH_HISTORY",
+  C4_AUTH_SERVER_INVALID: "C4_AUTH_SERVER_INVALID",
+  C4_AUTH_FLEXIBLE_POLICY: "C4_AUTH_FLEXIBLE_POLICY",
+  C4_AUTH_WATCH_IP_AUTH: "C4_AUTH_WATCH_IP_AUTH",
+  C4_AUTH_BLACK: "C4_AUTH_BLACK",
+  DEVELOP: "DEVELOP",
+  MODULE_VERSION: "MODULE_VERSION",
+  MODULE_VERSION_OPENED: "MODULE_VERSION_OPENED",
+  MODULE_VERSION_REPORTED: "MODULE_VERSION_REPORTED",
+  APP_GUARDING_HISTORY: "APPGUARDING_HISTORY",
+  HIDDEN_LOG: "HIDDEN_LOG",
+  USER: "user",
+  TEST: "test",
+} as const;
+
+export type MenuType = (typeof MENU_KEY)[keyof typeof MENU_KEY];
